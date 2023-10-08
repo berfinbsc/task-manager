77b2f98e48bdd9c74f383afa35f3d1002ce0c73b
berfin7 <berfinbasci4701@gmail.com>
2023-10-08 20:57:10 +0300

    rest apiler olusturuldu

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -32,20 +32,63 @@ res.status(500).json({msg:error})
 
 
 
-const getTask = (req,res)=>{
-    res.json({id:req.params.id})
+const getTask = async (req,res)=>{
+
+    try{
+        //const taskID = req.params.id;
+        const {id:taskID} = req.params 
+        const task = await Task.findOne({_id:taskID})
+     
+     if(!task){
+         return res.status(404).json({msg: `no task with id:${taskID}`})
+     }
+     res.status(200).json({task})
+    }
+catch(error){
+res.status(500).json({msg:error})
+}
 }
 
-const updateTask = (req,res)=>{
-    res.json({idm:req.params.id})
+const updateTask = async (req,res)=>{
+
+    try{
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new : true, //this options tells mongoose to return the updated document after the update is applied.
+            runValidators: true, //this options tells mongooge to return any  defined validators when performing the update.it ensure that the updated document adheres to the schema rules.
+        })
+        
+        if(!task){
+            return res.status(404).json({msg: `no task with id:${taskID}`})
+        }
+        res.status(200).json({task})
+
+    }
+    catch(error){
+res.status(500).json({msg : error})
+    }
+
 }
 
 
-const deleteTask = (req,res)=>{
-    res.send('all items from file')
-}
 
-//sincaplar fındık sever
+
+
+
+
+const deleteTask = async (req,res)=>{
+try{
+    const {id:taskID}=req.params
+    const task = await Task.findByIdAndDelete({_id : taskID})
+if(!task){
+   return res.status(404).json({msg:`no task with id : ${taskID}`})
+}
+res.status(200).json({task})
+}
+catch(error){
+    res.status(500).json({msg : error})
+}}
+
 
 module.exports={
     getAllTasks,getTask,createTask,deleteTask,updateTask
